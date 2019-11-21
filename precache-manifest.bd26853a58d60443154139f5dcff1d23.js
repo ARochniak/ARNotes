50dfb2a62314e6db065de6fe86fc1da95b474d9b
ARochniak <15bblinn@gmail.com>
2019-11-21 22:52:26 +0200

    Updates

---
+++ precache-manifest.bd26853a58d60443154139f5dcff1d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ec2e42305a24250ad56607cae2c980",
+    "url": "/ARNotes/index.html"
+  },
+  {
+    "revision": "b13fd8ced5459280632c",
+    "url": "/ARNotes/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5a5b83a807684dacea07",
+    "url": "/ARNotes/static/css/main.a1ce1f87.chunk.css"
+  },
+  {
+    "revision": "b13fd8ced5459280632c",
+    "url": "/ARNotes/static/js/2.bb6e1e7b.chunk.js"
+  },
+  {
+    "revision": "5a5b83a807684dacea07",
+    "url": "/ARNotes/static/js/main.c4e0a905.chunk.js"
+  },
+  {
+    "revision": "b90b3321a3b72d517aae",
+    "url": "/ARNotes/static/js/runtime-main.51f8a60c.js"
+  }
+]);
